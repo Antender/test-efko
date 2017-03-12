648f8cb1aef0fcc92d7950e03e5b601ff8d326f0
Antender <antender@live.ru>
2017-03-12 18:22:03 +0300

    Server is done

---
+++ api.js
@@ -0,0 +1,168 @@
+var mysql = require('promise-mysql')
+var bcrypt = require('bcrypt')
+var session = require('express-session')
+var MySQLStore = require('express-mysql-session')(session)
+var connection;
+
+function login(req,resp) {
+    if (!req.body || !req.body.username || !req.body.password) {
+        resp.sendStatus(403)
+        return
+    }
+    var userid
+    connection.query("SELECT Password, UserID FROM Users WHERE Username = ? LIMIT 1",[req.body.username])
+    .then(function(results,fields) {
+        if (results.length > 0) {
+            userid = results[0].UserID
+            return bcrypt.compare(req.body.password, results[0].Password)
+        } else {
+            return new Error()
+        }
+    })
+    .then(function(res) {
+        if (res) {
+            req.session.userid = userid
+            resp.sendStatus(200)
+        } else {
+            resp.sendStatus(403)
+        }
+    })
+}
+
+function logout(req, resp) {
+    req.session.userid = undefined
+    resp.sendStatus(200)
+}
+
+function createUser(req,resp) {
+    if (!req.body || !req.body.username || !req.body.password || 
+        typeof(req.body.canSolve) == 'undefined' || 
+        typeof(req.body.canReview) == 'undefined') {
+        resp.sendStatus(403)
+        return
+    }
+    bcrypt.hash(req.body.password,5)
+    .then(function(password) {
+        return connection.query("INSERT INTO Users (Username, Password, CanSolve, CanReview) VALUES (?,?,?,?)",[
+            req.body.username, password, !!req.body.canSolve, !!req.body.canReview
+        ])
+    })
+    .then(function(results,fields) {
+        resp.sendStatus(200)
+    },function(e) {
+        resp.sendStatus(403)
+    })
+}
+
+function addProblem(req,resp) {
+    if (!req.body || typeof(req.session.userid) != 'number') {
+        resp.sendStatus(403)
+        return
+    }
+    checkAuthorization(req, resp, 'canSolve', function() {
+        connection.query('INSERT INTO Solutions (ProblemText, SolutionText, SolutionScore) VALUES ("","",1)')
+        .then(function(results, fields) {
+            resp.json(results.insertId)
+        })
+    })
+}
+
+function checkAuthorization(req, resp, permission, callback) {
+    //Query escaping not working properly
+    connection.query("SELECT UserID FROM Users WHERE UserID = " + req.session.userid + " AND " + permission + " = TRUE LIMIT 1")
+    .then(function(results,fields) {
+        if (results[0]) {
+            return callback()
+        } else {
+            resp.sendStatus(403)
+            return new Error()
+        }
+    })
+}
+
+function updateSolution(field, permission) {
+    return function(req, resp) {
+        if (!req.body || !req.body[field] || typeof(req.body.solutionid) != 'number' || typeof(req.session.userid) != 'number') {
+            resp.sendStatus(403)
+            return
+        }
+        checkAuthorization(req, resp, permission, function() {
+            connection.query('UPDATE Solutions SET ' + field + ' = ? WHERE SolutionID = ?',[req.body[field], req.body.solutionid])
+            .then(function(results, fields) {
+                resp.sendStatus(200)
+            },function(e){
+                resp.sendStatus(404)
+            })
+        })
+    }
+}
+
+function getData(req, resp) {
+    if (typeof(req.session.userid) != 'number') {
+        resp.sendStatus(403)
+        return
+    }
+    checkAuthorization(req, resp, 'TRUE', function (e) {
+        connection.query('SELECT * FROM Solutions')
+        .then(function(results, fields) {
+            resp.json(results.map(function(row) {
+                return {
+                    'solutionid' : row.SolutionID,
+                    'problemText' : row.ProblemText,
+                    'solutionText' : row.SolutionText,
+                    'solutionScore' : row.SolutionScore
+                }
+            }))
+        })
+    })
+}
+
+function loggedIn(req) {
+    return typeof(req.session.userid) != 'undefined'
+}
+
+exports.init = function(app,callback) {
+    if (typeof(process.env.DATABASE_URL) == 'undefined') {
+        console.log("No DATABASE_URL found")
+        process.exit()
+    }
+    mysql.createConnection(process.env.DATABASE_URL)
+    .then(function(conn) {
+        connection = conn;
+        return connection.query("CREATE TABLE IF NOT EXISTS Users (UserID BIGINT NOT NULL AUTO_INCREMENT, PRIMARY KEY (UserID),\
+Username VARCHAR(32), UNIQUE INDEX (Username), Password TEXT, CanSolve BOOLEAN, CanReview BOOLEAN)")
+    },function(e){
+        console.log("Database error:" + process.env.DATABASE_URL)
+        process.exit()
+    })
+    .then(function(results, fields) {
+        return connection.query("CREATE TABLE IF NOT EXISTS Solutions (SolutionID BIGINT NOT NULL AUTO_INCREMENT,\
+PRIMARY KEY (SolutionID), ProblemText TEXT, SolutionText TEXT, SolutionScore TINYINT(5))")
+    }, function(e){
+        console.log("Database error:" + process.env.DATABASE_URL)
+        process.exit()
+    })
+    .then(function(results,fields) {
+        //Routes
+        sessionStore = new MySQLStore({},connection)
+        app.use(session({
+            key: 'session',
+            secret: process.env.SESSION_SECRET,
+            store: sessionStore,
+            resave: false,
+            saveUninitialized: true
+        }))
+        app.post('/login', login)
+        app.get('/logout', logout)
+        app.post('/createUser', createUser)
+        app.get('/addProblem', addProblem)
+        app.post('/updateSolutionText', updateSolution('solutionText','canSolve'))
+        app.post('/updateProblemText',updateSolution('problemText','canSolve'))
+        app.post('/updateSolutionScore',updateSolution('solutionScore','canReview'))
+        app.get('/getData',getData)
+        return callback(connection)
+    }, function(e){
+        console.log("Database error:" + process.env.DATABASE_URL)
+        process.exit()
+    });
+}
--- index.js
+++ index.js
@@ -1,27 +1,16 @@
 //Requirements
-var express = require('express');
-var mysql = require('promise-mysql');
-var connection;
+var express = require('express')
+var app = express()
+var api = require('./api.js')
+var bodyParser = require('body-parser');
 
 //Initialization
-var app = express();
+var sessionStore
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public')); 
-mysql.createConnection(process.env.DATABASE_URL).then(function(conn){
-    connection = conn;
-}).error(function(e){
-    console.log(process.env.DATABASE_URL)
-    process.exit()
-});
-
-//Routes
-app.get('/', function(request, response) {
-    connection.query('SELECT "Hello, world!" AS solution')
-    .then(function(rows,fields) {
-        response.send(rows[0].solution)
-    })
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+app.use(bodyParser.json())
+api.init(app,function(connection) {
+    app.listen(app.get('port'), function() {
+        console.log('Node app is running on port', app.get('port'));
+    });
+})
